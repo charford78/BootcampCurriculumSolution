2f42fec911defde10a107ba3eedfd58ac864bead
Charlie Ford <charles.ford1978@gmail.com>
2021-09-28 13:52:13 -0400

    Add project files.

---
+++ BootcampCurriculum/Topic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootcampCurriculum
+{
+    class Topic
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Difficulty { get; set; }
+        private static int NextId { get; set; } = 1;
+
+        
+        public void Print()
+        {
+            Console.WriteLine($"{Id} | {Name} | {Category} | {Difficulty}");
+        }
+
+        public Topic()
+        {
+            this.Id = NextId++;
+        }
+    }
+}
